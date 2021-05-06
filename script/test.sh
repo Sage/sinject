3b23e1ce07363cbee0087b6c4eb1f58f89d1b88b
Ricard Forniol Agustí <ricard.forniol@sage.com>
2021-05-06 12:51:52 +0200

    Update ruby version on docker runner environment

--- script/test.sh
+++ script/test.sh
@@ -5,4 +5,4 @@ docker-compose up -d
 
 spec_path=spec/$1
 
-docker exec -it gem_test_runner bash -c "cd gem_src && bundle install && bundle exec rspec $*"
+docker exec -it gem_test_runner sh -c "cd gem_src && bundle install && bundle exec rspec $*"
